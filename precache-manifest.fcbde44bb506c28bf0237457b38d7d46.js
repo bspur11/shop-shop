960dcb8b5ea7f97a8c9dd0720f055680cd3b0b29
Brad Spurrell <63411329+bspur11@users.noreply.github.com>
2021-05-11 22:17:15 -0400

    Updates

---
+++ precache-manifest.fcbde44bb506c28bf0237457b38d7d46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7ecb15a7d6f1f69ee38b677a40364c",
+    "url": "/shop-shop/index.html"
+  },
+  {
+    "revision": "47d265a9fd5de374dd98",
+    "url": "/shop-shop/static/css/main.b38a3b8e.chunk.css"
+  },
+  {
+    "revision": "72978be2da184a82d7bd",
+    "url": "/shop-shop/static/js/2.ffb89eef.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/shop-shop/static/js/2.ffb89eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47d265a9fd5de374dd98",
+    "url": "/shop-shop/static/js/main.87bbeb85.chunk.js"
+  },
+  {
+    "revision": "8b03114d0dc23cbcbad9",
+    "url": "/shop-shop/static/js/runtime-main.e6fc2988.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/shop-shop/static/media/spinner.ab497855.gif"
+  }
+]);
